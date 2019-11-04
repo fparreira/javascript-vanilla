fa852e9c109e18ab5644432c45f07c94ad6619a1
Fernando Parreira <fparreira@gmail.com>
2019-11-04 09:41:34 -0300

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript-vanilla
-Vanilla JS 
+## Vanilla JS 
+
+Some practice codes and challenge resolution from course **The Complete JavaScript Course 2019: Build Real Projects!** by Udemy - Jonas Schmedtmann
+
--- public/dom/app.js
+++ public/dom/app.js
@@ -15,13 +15,17 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
 
 var x = document.querySelector("#score-0").textContent;
 
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector(".btn-roll").addEventListener('click', function(){
 
@@ -31,6 +35,17 @@ document.querySelector(".btn-roll").addEventListener('click', function(){
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + ".png";
 
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    }else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+        
+    }
 
 
 });
