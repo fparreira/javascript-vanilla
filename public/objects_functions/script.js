209d4ac3e00b5bde30e71e85a65fad8e8acfda3e
Fernando Parreira <fparreira@gmail.com>
2019-11-08 17:01:40 -0300

    objects and functions updated

--- public/objects_functions/script.js
+++ public/objects_functions/script.js
@@ -1,26 +1,44 @@
 
+// Objects and functions
+
 // var john = {
 //     'name' : 'John',
 //     'birth' : 1990
 // }
 
-var Person = function(name, birth, job){
-    this.name = name,
-    this.birth = birth,
-    this.job = job
+
+// var Person = function(name, birth, job){
+//     this.name = name,
+//     this.birth = birth,
+//     this.job = job
+// }
+
+// Person.prototype.calcAge = function(){
+//     console.log(2019 - this.birth);
+// }
+
+// Person.prototype.lastName = 'Parreira';
+
+// var john = new Person('fernando', 1977, 'ti');
+
+// john.calcAge();
+
+// console.log(john.lastName);
+
+// //prototype chains - inheritance
+// console.log(john);
+
+
+// Objects.create
+var personProto = {
+    calcAge: function(){
+        console.log(2016-birth);        
+    }
 }
 
-Person.prototype.calcAge = function(){
-    console.log(2019 - this.birth);
-}
+var john = Object.create(personProto)
+john.name = 'Joao';
+john.birth = 2015;
+john.job = 'estudent';
 
-Person.prototype.lastName = 'Parreira';
-
-var john = new Person('fernando', 1977, 'ti');
-
-john.calcAge();
-
-console.log(john.lastName);
-
-//prototype chains - inheritance
-console.log(john);
+var luke = Object.create(Object.prototype)
