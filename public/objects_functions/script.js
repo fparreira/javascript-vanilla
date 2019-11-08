0c51b7959ad6db56d87631d10f0954f41759e04b
Fernando Parreira <fparreira@gmail.com>
2019-11-08 08:59:10 -0300

    objects and functions updated

--- public/objects_functions/script.js
+++ public/objects_functions/script.js
@@ -1,11 +1,26 @@
 
-var john = {
-    'name' : 'John',
-    'birth' : 1990
+// var john = {
+//     'name' : 'John',
+//     'birth' : 1990
+// }
+
+var Person = function(name, birth, job){
+    this.name = name,
+    this.birth = birth,
+    this.job = job
 }
 
+Person.prototype.calcAge = function(){
+    console.log(2019 - this.birth);
+}
 
-console.log(john.name);
+Person.prototype.lastName = 'Parreira';
+
+var john = new Person('fernando', 1977, 'ti');
+
+john.calcAge();
+
+console.log(john.lastName);
 
 //prototype chains - inheritance
-console.log(john.hasOwnProperty);
+console.log(john);
