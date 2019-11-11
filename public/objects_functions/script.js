258167415722c6bccced704a10c6cabede7e136f
Fernando Parreira <fparreira@gmail.com>
2019-11-11 07:51:07 -0300

    objects and functions updated

--- public/objects_functions/script.js
+++ public/objects_functions/script.js
@@ -29,16 +29,58 @@
 // console.log(john);
 
 
-// Objects.create
-var personProto = {
-    calcAge: function(){
-        console.log(2016-birth);        
-    }
+// // Objects.create
+// var personProto = {
+//     calcAge: function(){
+//         console.log(2016-birth);        
+//     }
+// }
+
+// var john = Object.create(personProto)
+// john.name = 'Joao';
+// john.birth = 2015;
+// john.job = 'estudent';
+
+// var luke = Object.create(Object.prototype)
+
+
+// Primitive vs Objects
+// primitives
+var a = 23;
+var b = a;
+
+a = 42;
+console.log(a);
+console.log(b);
+
+// objects
+var obj1 = {
+    name: 'John',
+    age: 10
 }
 
-var john = Object.create(personProto)
-john.name = 'Joao';
-john.birth = 2015;
-john.job = 'estudent';
+var obj2 = obj1;
 
-var luke = Object.create(Object.prototype)
+obj1.age = 11;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+// functions
+var age = 10;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+}
+
+function change(x, y){
+    x = 30;
+    y.city = 'San Francisco'
+}
+
+change(age, obj);
+
+console.log(age, obj.city);
+
+
+
