57e3832e3e9499734332f92088462754e0f8ee11
Fernando Parreira <fparreira@gmail.com>
2020-02-20 12:48:45 -0300

    busget project update

--- public/budgety/app.js
+++ public/budgety/app.js
@@ -13,6 +13,16 @@ var budgetController = (function(){
         this.value = value;
     };
 
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function(cur){
+            sum += cur.value;
+        });
+
+        data.totals[type] = sum;
+        
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +31,9 @@ var budgetController = (function(){
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     }
 
     return {
@@ -48,10 +60,30 @@ var budgetController = (function(){
 
         calculateBudget: function(){
 
+            // calculate total income and expanses
+            calculateTotal('exp');
+            calculateTotal('inc');
             
-
+            // calculate the budget: income - expanses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // calculate the percentage of income that we spent
+            if(data.totals.inc > 0 ){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            }else {
+                data.percentage = -1;
+            }
 
         },
+
+        getBudget: function(){
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        }, 
 
         testing : function(){
             console.log(data);
@@ -73,7 +105,11 @@ var UIController = (function(){
         inputValue: '.add__value',
         inputBtn: '.add__btn', 
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expansesLabel: '.budget__expanses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -122,9 +158,16 @@ var UIController = (function(){
 
         },
 
+        displayBudget: function(obj){
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
+
     }
 
 })();
@@ -147,13 +190,17 @@ var controller = (function(budgetCtrl, UICtrl){
     };
 
     var updateBudget = function(){
+        var budget;
 
         // calculate the budget
-
+        budgetCtrl.calculateBudget();
 
         // return the budget
+        budget = budgetCtrl.getBudget();
 
         // display the budget on the UI
+        console.log(budget);
+        
 
     }
 
