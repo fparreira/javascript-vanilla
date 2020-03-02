739b930a4a17e595f53d34cfefee1ab0cc3e40bf
Fernando Parreira <fparreira@gmail.com>
2020-03-02 12:41:06 -0300

    budget project updated

--- public/budgety/app.js
+++ public/budgety/app.js
@@ -246,6 +246,25 @@ var UIController = (function(){
 
         },
 
+        formatNumber: function(num, type){
+            var numSplit, int, dec;
+
+            num = Math.abs(num);
+            num = num.toFixed(2);
+
+            numSplit = num.split(".");
+
+            int = numSplit[0];
+            if(int.length > 3){
+                int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+            }
+
+            dec = numSplit[1];
+
+            return (type === 'exp' ? sign = "-" : sign = '+') + int + dec;
+
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
